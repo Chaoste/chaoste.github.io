8c4f0a5bf4c741e6f0a21bb8b30d568640bb81c7
Thomas Kellermeier <t-kellermeier@hotmail.de>
2017-11-23 00:16:28 +0100

    restructure css setup to make none scrolling in modal possible

--- src/content/preview/Image.js
+++ src/content/preview/Image.js
@@ -7,19 +7,28 @@ class Image extends Component {
     open: false,
   };
 
-  onClick = event => {};
+  enablePreview = () => {
+    this.setState({
+      open: true,
+    });
+  };
+
+  disablePreview = () => {
+    this.setState({
+      open: false,
+    });
+  };
 
   render() {
     return (
       <div className="imageContainer">
-        <a className="thumbnailContainer" tabIndex={0} onClick={this.onClick}>
-          <img
-            className="thumbnail"
-            alt="Project Preview"
-            src={this.props.info.image}
-          />
+        <a tabIndex={0} onClick={this.enablePreview}>
+          <img alt="Project Preview" src={this.props.info.image} />
         </a>
-        <div className={`modalWrapper ${this.state.open ? 'open' : 'hidden'}`}>
+        <div
+          className={`modalWrapper ${this.state.open ? 'open' : 'hidden'}`}
+          onClick={this.disablePreview}
+        >
           <img
             className="modalDialog"
             alt="Project Preview"
