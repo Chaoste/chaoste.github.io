efb0327e30888f7315a9674dcfbd75d5d44352a7
Thomas Kellermeier <t-kellermeier@hotmail.de>
2017-11-24 08:49:34 +0100

    description for mas quickpark

--- src/content/projects/14-MASQuickpark.jsx
+++ src/content/projects/14-MASQuickpark.jsx
@@ -16,8 +16,20 @@ export class Content extends Component {
     return (
       <div>
         <p>
-          [MIssing content !!! :O ] quick park bla bla bla sdf sdf dsf dsf dsf
-          sdf sdf sdfsdf asdf dsf dsf dsf afsdf asdf asdf dsf
+          This Bachelor thesis presents a Multi-agent System based on the
+          problem definition of the Quickpark paper. It proposed algorithms to
+          generate parking search routes based on probabilistic information. In
+          this project I tackled the theoretical problem of multiple drivers
+          arriving at their destinations and starting their parking search.
+          Their positions are close to each other so the probability of finding
+          a parking spot in a street can be collected by different agents.
+        </p>
+        <p>
+          This leads to a game theoretical problem in which agents play against
+          eachother to collect a streets probability before the others. After
+          defining the problem and a cost function I present several algorithms
+          generating routes for each agent. They are optimized on the distance,
+          search time and fairness among the agents.
         </p>
       </div>
     );
