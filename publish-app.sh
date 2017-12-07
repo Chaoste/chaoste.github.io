cfb1205e6932302de2eb7d8977ea43b82fd8e88b
Thomas Kellermeier <t-kellermeier@hotmail.de>
2017-12-07 16:36:59 +0100

    Add comments to publishing script

--- publish-app.sh
+++ publish-app.sh
@@ -1,13 +1,20 @@
+# Make sure to be up to date
 git fetch --all
-rm -rf icons static
+
+# Remove old build folders
+rm -rf icons static build
 
 yarn build
+
+# Move build files to main directory
 cd build
 mv ./static ../static
 mv ./icons ../icons
 mv * ..
 cd ..
 rm -r build
+
+# Create and push new commit
 git add .
 git commit -m "Publish new version"
 git push
