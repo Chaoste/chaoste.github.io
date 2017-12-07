5567e657b1af346154dd21a34e90369dde7e7882
Thomas Kellermeier <t-kellermeier@hotmail.de>
2017-12-07 15:25:49 +0100

    fix publish script

--- publish-app.sh
+++ publish-app.sh
@@ -1,10 +1,12 @@
 git fetch --all
 git checkout gh-pages
+git merge origin/master
 yarn build
 cd build
 mv * ..
 cd ..
+rm build
 git add .
 git commit -m "Publish new version"
-git publish
+git push
 git checkout master
