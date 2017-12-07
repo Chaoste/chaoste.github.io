757e72a95715f476f3bc62a0aebd38d71761e0aa
Thomas Kellermeier <t-kellermeier@hotmail.de>
2017-12-07 16:42:07 +0100

    Show Home if no route matches (fices Android Shortcut)

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
             <Route path="/about" component={About} />
             <Route exact path="/projects" component={Projects} />
             <Route path="/projects/:category" component={ProjectsCategory} />
+            <Route path="*" component={Home} />
           </AnimatedSwitch>
           <Footer />
         </div>
